6be0a8cca5284fb48507e508cf2b6364cc68576d
Joe-Pena <joepena@protonmail.com>
2019-04-03 10:29:16 -0600

    Top Bar done

---
+++ src/components/TopBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/Base.scss';
+
+class TopBar extends React.Component {
+  render() {
+    return(
+      <div className='top-bar'>
+        <h1 className='top-bar_heading'>LOVELORN REGAL</h1>
+        <p className='top-bar_accolade'>"2012 Colorado solo artist of the year" -Bridging the Music</p>
+      </div>
+    )
+  }
+}
+
+export default TopBar;
