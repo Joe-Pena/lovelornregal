6f56b4aba5ae268944142967d265cb2e2fd9ed90
Joe-Pena <joepena@protonmail.com>
2019-04-04 20:18:25 -0600

    Add navbar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -5,8 +5,15 @@ class TopBar extends React.Component {
   render() {
     return(
       <div className='top-bar'>
-        <h1 className='top-bar_heading'>LOVELORN REGAL</h1>
-        <p className='top-bar_accolade'>"2012 Colorado solo artist of the year" -Bridging the Music</p>
+        <div className="top-bar--navbar">
+            <li className="top-bar--navbar_link">Home</li>
+            <li className="top-bar--navbar_link">About</li>
+            <li className="top-bar--navbar_link">Contact</li>
+        </div>
+        <div className='top-bar_info'>
+          <h1 className='top-bar_info--heading'>LOVELORN REGAL</h1>
+          <p className='top-bar_info--accolade'>"2012 Colorado solo artist of the year" -Bridging the Music</p>
+        </div>
       </div>
     )
   }
