57c90b57dcd71952dc901c719d3b72622b3a733b
Joe-Pena <joepena@protonmail.com>
2019-04-04 21:22:28 -0600

    Add media player

--- src/components/MiddleBar.js
+++ src/components/MiddleBar.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import '../styles/Base.scss';
+import Blue from '../music/Blue.mp3';
+import AType from '../music/A-Type.mp3';
+import HisHand from '../music/HisTalkingHands.mp3';
+import AlbumCover from '../images/albumcover.jpg';
+
+import MusicPlayer from 'react-responsive-music-player';
+
+const playlistMusic = [
+  {
+    url: Blue,
+    cover: AlbumCover,
+    title: 'Blue',
+    artist: ['Lovelorn Regal']
+  },
+  {
+    url: AType,
+    cover: AlbumCover,
+    title: 'A-Type',
+    artist: ['Lovelorn Regal']
+  },
+  {
+    url: HisHand,
+    cover: AlbumCover,
+    title: 'His Talking Hands',
+    artist: ['Lovelorn Regal']
+  }
+]
 
 class MiddleBar extends React.Component {
+
   render() {
     return (
       <div className="playback-section">
+        <MusicPlayer playlist={playlistMusic} />
       </div>
     )
   }
